3e937e024a288cc31d54e80abf30992fea06aba1
Ben Keys <bkeys818@gmail.com>
2023-10-15 10:03:54 -0400

    ✨ Create non-functional menu bar

---
+++ src/main/java/edu/bsu/cs222/markdownEditor/MenuBarController.java
@@ -0,0 +1,22 @@
+package edu.bsu.cs222.markdownEditor;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MenuBarController {
+    @FXML
+    private void newFile(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void openFile(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void saveFile(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void saveFileAs(ActionEvent actionEvent) {
+    }
+}
