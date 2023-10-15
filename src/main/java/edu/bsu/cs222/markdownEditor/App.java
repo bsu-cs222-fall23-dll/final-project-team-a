44eabe3b4c3948ac19b6fe5ea5a846116a504c9f
Ben Keys <bkeys818@gmail.com>
2023-10-15 09:59:25 -0400

    🚧 Clean up empty view

--- src/main/java/edu/bsu/cs222/markdownEditor/App.java
+++ src/main/java/edu/bsu/cs222/markdownEditor/App.java
@@ -8,16 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/app.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1120, 680);
+        stage.setTitle("Markdown Editor");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
