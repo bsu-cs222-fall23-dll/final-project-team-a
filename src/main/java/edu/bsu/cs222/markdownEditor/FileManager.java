848e4381c86cc6fe55011c3851ed07ac49231a5d
Evan Kiser <evan.kiser@bsu.edu>
2023-10-16 17:16:07 -0400

    ✅ Update FileManager to pass tests.

--- src/main/java/edu/bsu/cs222/markdownEditor/FileManager.java
+++ src/main/java/edu/bsu/cs222/markdownEditor/FileManager.java
@@ -1,6 +1,9 @@
 package edu.bsu.cs222.markdownEditor;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileManager {
@@ -10,15 +13,36 @@ public class FileManager {
         this.activeFilePath = activeFilePath;
     }
 
-    public String getActiveFileContents() {
-        return null;
+    public String getActiveFileContents() throws NoFileOpenException {
+        if (activeFilePath == null) throw new NoFileOpenException();
+        try {
+            return Files.readString(activeFilePath, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void open(File file) {}
+    public void open(File file) {
+        activeFilePath = file.toPath();
+    }
 
-    public void save(String content) {}
+    public void save(String content) throws NoFileOpenException {
+        if (activeFilePath == null) throw new NoFileOpenException();
+        try {
+            Files.writeString(activeFilePath, content, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-    public void saveAs(String content, File file) {}
+    public void saveAs(String content, File file) {
+        try {
+            Files.writeString(file.toPath(), content, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        open(file);
+    }
 }
 
 class NoFileOpenException extends Exception {
