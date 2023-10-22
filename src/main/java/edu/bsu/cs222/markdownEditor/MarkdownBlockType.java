1cba8af91cdff440f8b6b2670348a227d23e754b
Ben Keys <bkeys818@gmail.com>
2023-10-22 11:49:40 -0400

    🚸 Show rendered markdown like bullet points

--- src/main/java/edu/bsu/cs222/markdownEditor/MarkdownBlockType.java
+++ src/main/java/edu/bsu/cs222/markdownEditor/MarkdownBlockType.java
@@ -8,13 +8,18 @@ import java.util.regex.Pattern;
 public enum MarkdownBlockType {
     Paragraph(null, "p"),
     Heading1("#", "h1"),
-    List("-", "ul");
+    List("-", "•", "ul");
 
     public final String regexp;
-    private final String markdown, className;
+    private final String markdown, renderedText, className;
 
     MarkdownBlockType(String markdown, String className) {
+        this(markdown, null, className);
+    }
+
+    MarkdownBlockType(String markdown, String renderedText, String className) {
         this.markdown = markdown;
+        this.renderedText = renderedText;
         this.className = className;
         this.regexp = markdown == null ? null : "^" + Pattern.quote(markdown) + " .*";
     }
@@ -31,9 +36,11 @@ public enum MarkdownBlockType {
 
     public void removeMarkdown(CodeArea block) {
         if (markdown != null) block.deleteText(0, markdown.length() + 1);
+        if (renderedText != null) block.insertText(0, renderedText + " ");
     }
 
     public void addMarkdown(CodeArea block) {
+        if (renderedText != null) block.deleteText(0, renderedText.length() + 1);
         if (markdown != null) {
             block.insertText(0, markdown + " ");
             block.setStyleClass(0, markdown.length() + 1, "md");
