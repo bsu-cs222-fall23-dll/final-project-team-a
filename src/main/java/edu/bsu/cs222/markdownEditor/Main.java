98b142155e812d027b47141b44c65978e9872c00
Ben Keys <bkeys818@gmail.com>
2023-10-27 18:11:24 -0400

    🚧 Create custom MarkdownEditor

--- src/main/java/edu/bsu/cs222/markdownEditor/Main.java
+++ src/main/java/edu/bsu/cs222/markdownEditor/Main.java
@@ -1,13 +1,8 @@
 package edu.bsu.cs222.markdownEditor;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.Objects;
 
 public class Main extends Application {
     public static FileManager fileManager = new FileManager(null);
@@ -17,19 +12,15 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getResourceUrl("/app.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1120, 680);
-        scene.getStylesheets().add(getResourceUrl("/markdown.css").toExternalForm());
+    public void start(Stage stage) {
+        MarkdownEditor markdownEditor = new MarkdownEditor();
+//        VirtualizedScrollPane<MarkdownEditor> vsPane = new VirtualizedScrollPane<>(markdownEditor);
+//        Scene scene = new Scene(vsPane, 1120, 680);
+        Scene scene = new Scene(markdownEditor, 1120, 680);
         stage.setTitle("Markdown Editor");
         stage.setScene(scene);
         stage.show();
-        org.scenicview.ScenicView.show(scene); // for debugging
-    }
-
-    private URL getResourceUrl(String name) {
-        URL url = getClass().getResource(name);
-        return Objects.requireNonNull(url);
+        org.scenicview.ScenicView.show(scene);
     }
 
 }
---
+++ src/main/java/edu/bsu/cs222/markdownEditor/MarkdownEditor.java
@@ -0,0 +1,49 @@
+package edu.bsu.cs222.markdownEditor;
+
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.text.TextFlow;
+import org.fxmisc.richtext.GenericStyledArea;
+import org.fxmisc.richtext.TextExt;
+import org.fxmisc.richtext.model.SegmentOps;
+import org.fxmisc.richtext.model.StyledSegment;
+import org.fxmisc.richtext.model.TextOps;
+
+import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.function.Consumer;
+
+public class MarkdownEditor extends GenericStyledArea<Void, String, Collection<String>> {
+
+    private static final TextOps<String, Collection<String>> STYLED_TEXT_OPS = SegmentOps.styledTextOps();
+
+    public MarkdownEditor() {
+        super(null, MarkdownEditor::applyParagraphStyle, Collections.emptyList(), STYLED_TEXT_OPS, MarkdownEditor::nodeFactory);
+        getStyleClass().add("markdown-editor");
+        linkCss();
+    }
+
+    private static void applyParagraphStyle(TextFlow textFlow, Void paragraphStyle) {
+    }
+
+    private static Node nodeFactory(StyledSegment<String, Collection<String>> styledSegment) {
+        String text = styledSegment.getSegment();
+        TextExt textNode = createStyledTextNode(textExt -> textExt.setText(text));
+        textNode.getStyleClass().addAll(styledSegment.getStyle());
+        return textNode;
+    }
+
+    public static TextExt createStyledTextNode(Consumer<TextExt> consumer) {
+        TextExt textExt = new TextExt();
+        textExt.setTextOrigin(VPos.TOP);
+        consumer.accept(textExt);
+        return textExt;
+    }
+
+    private void linkCss() {
+        URL url = MarkdownEditor.class.getResource("/markdown.css");
+        if (url == null) throw new RuntimeException("Couldn't find CSS file");
+        getStylesheets().add(url.toExternalForm());
+    }
+}
