094aedb84beb40e920b35cb5cc3fa1d42727e216
Ben Keys <bkeys818@gmail.com>
2023-11-02 10:41:26 -0400

    🚨 Fix Main.java

--- src/main/java/edu/bsu/cs222/markdownEditor/Main.java
+++ src/main/java/edu/bsu/cs222/markdownEditor/Main.java
@@ -1,8 +1,13 @@
 package edu.bsu.cs222.markdownEditor;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class Main extends Application {
     public static FileManager fileManager = new FileManager(null);
@@ -12,15 +17,19 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
-        MarkdownTextArea textArea = new MarkdownTextArea();
-//        VirtualizedScrollPane<MarkdownEditor> vsPane = new VirtualizedScrollPane<>(markdownEditor);
-//        Scene scene = new Scene(vsPane, 1120, 680);
-        Scene scene = new Scene(textArea, 1120, 680);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getResourceUrl("/app.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1120, 680);
+        scene.getStylesheets().add(getResourceUrl("/markdown.css").toExternalForm());
         stage.setTitle("Markdown Editor");
         stage.setScene(scene);
         stage.show();
-        org.scenicview.ScenicView.show(scene);
+        org.scenicview.ScenicView.show(scene); // for debugging
+    }
+
+    private URL getResourceUrl(String name) {
+        URL url = getClass().getResource(name);
+        return Objects.requireNonNull(url);
     }
 
 }
