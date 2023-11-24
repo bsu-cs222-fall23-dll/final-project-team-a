ead4188a26b7238b5ebb5f9f16dab1845593469c
Ben Keys <bkeys818@gmail.com>
2023-11-24 18:47:41 -0500

    🔧 Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module edu.bsu.cs222.markdownEditor {
+    requires org.fxmisc.flowless;
+    requires org.fxmisc.richtext;
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.swing;
+
+    exports edu.bsu.cs222.markdownEditor;
+}
