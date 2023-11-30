a636e0b3bf842cb20a01347de2faadd0833614e3
paytonh13 <payton.hutsell@gmail.com>
2023-11-30 13:10:14 -0500

    ✅ Created Test for Heading2 SyntaxReference

---
+++ src/test/java/edu/bsu/cs222/markdownEditor/parser/Heading2SyntaxReferenceTest.java
@@ -0,0 +1,35 @@
+package edu.bsu.cs222.markdownEditor.parser;
+
+import edu.bsu.cs222.markdownEditor.textarea.ParagraphStyle;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Heading2SyntaxReferenceTest {
+
+    private final ParagraphSyntaxReference syntaxReference;
+
+    Heading2SyntaxReferenceTest() {
+        String markdown = "## Hello cruel world!";
+        LineParser parser = new LineParser(markdown);
+        List<SyntaxReference> syntaxReferences = parser.getSyntaxReferences();
+        if (syntaxReferences.get(0) instanceof ParagraphSyntaxReference paragraphSyntaxReference) {
+            syntaxReference = paragraphSyntaxReference;
+        } else {
+            throw new AssertionError("SyntaxReference is the wrong type");
+        }
+    }
+
+    @Test
+    public void typeTest() {
+        assertEquals(ParagraphStyle.Heading2, syntaxReference.paragraphStyle);
+    }
+
+    @Test
+    public void testTag() {
+        assertEquals("## ", syntaxReference.syntax);
+    }
+
+}
