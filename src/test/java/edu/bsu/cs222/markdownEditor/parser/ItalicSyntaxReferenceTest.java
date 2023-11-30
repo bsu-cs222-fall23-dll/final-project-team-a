d8000381338f74d54a37e0778671ffed8d497c46
paytonh13 <payton.hutsell@gmail.com>
2023-11-30 12:49:09 -0500

    ✅ Created Test for Italic SyntaxReference

---
+++ src/test/java/edu/bsu/cs222/markdownEditor/parser/ItalicSyntaxReferenceTest.java
@@ -0,0 +1,43 @@
+package edu.bsu.cs222.markdownEditor.parser;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ItalicSyntaxReferenceTest {
+
+    private final TagWrappedSyntaxReference syntaxReference;
+
+    ItalicSyntaxReferenceTest() {
+        String markdown = "Hello *cruel* world!";
+        LineParser parser = new LineParser(markdown);
+        List<SyntaxReference> syntaxReferences = parser.getSyntaxReferences();
+        if (syntaxReferences.get(0) instanceof TagWrappedSyntaxReference tagWrappedSyntaxReference) {
+            syntaxReference = tagWrappedSyntaxReference;
+        } else {
+            throw new AssertionError("SyntaxReference is the wrong type");
+        }
+    }
+
+    @Test
+    public void typeTest() {
+        assertEquals(InlineSyntaxType.Italic, syntaxReference.type);
+    }
+
+    @Test
+    public void testTag() {
+        assertEquals("*", syntaxReference.tag);
+    }
+
+    @Test
+    public void testText() {
+        assertEquals("cruel", syntaxReference.getText());
+    }
+
+    @Test
+    public void testStartPosition() {
+        assertEquals(6, syntaxReference.start);
+    }
+}
