c1788256906e46e6c7d5575df2b45f5ee87bcf7d
Ben Keys <bkeys818@gmail.com>
2023-12-07 13:11:06 -0500

    🐛 Fix url decoding bug

--- src/main/java/edu/bsu/cs222/markdownEditor/PreferenceController.java
+++ src/main/java/edu/bsu/cs222/markdownEditor/PreferenceController.java
@@ -10,17 +10,26 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.Arrays;
 import java.util.List;
 
 public class PreferenceController {
 
-    private final Path stylesDirectory = Path.of(Main.getResourceUrl("/styles").getPath());
+    private final Path stylesDirectory;
     private AppController appController;
 
     @FXML private ComboBox<String> fontComboBox;
     @FXML private ComboBox<String> styleSheetComboBox;
+
+    public PreferenceController() {
+        URL url = Main.getResourceUrl("/some styles");
+        String decodedUrlPath = URLDecoder.decode(url.getPath(), StandardCharsets.UTF_8);
+        stylesDirectory = Path.of(decodedUrlPath);
+        System.out.println(stylesDirectory);
+    }
 
     public void setAppController(AppController appController) {
         this.appController = appController;
@@ -76,7 +85,7 @@ public class PreferenceController {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSS files (*.css)", "*.css"));
         File file = fileChooser.showOpenDialog(null);
-        if (file.getName().equals("styles/default.css")) return;
+        if (file.getName().equals("some styles/default.css")) return;
         Path newPath = stylesDirectory.resolve(file.getName());
         try {
             Files.copy(file.toPath(), newPath, StandardCopyOption.REPLACE_EXISTING);
